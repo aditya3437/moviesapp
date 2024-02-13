460e49559613f365c53e4f4a6f99ea62745300df
aditya3437 <chaudharyaditya5556@gmail.com>
2024-02-13 22:11:02 +0530

    MoviesApps

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,38 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./header.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    let uname = localStorage.getItem("username");
+    if (uname) {
+      uname = uname?.split("@")[0];
+      setUsername(uname);
+    }
+  }, [username]);
+
+  const Logout = ()=>{
+    localStorage.clear("username");
+    setUsername("");
+  }
   return (
     <div>
-    <header>
-      <div className='container flexSB'>
-        <nav className='flexSB'>
-          <div className='logo'>
-            <img src='./Images/logo.png' alt='' />
+      <header>
+        <div className="container flexSB">
+          <nav className="flexSB">
+            <div className="logo">
+              <img src="./Images/logo.png" alt="" />
+            </div>
+            <ul>
+              <Link to="/">Home</Link>
+              <Link to="/series">Series</Link>
+              <Link to="/movies">Movies</Link>
+              <Link to="/contact">Contact</Link>
+            </ul>
+            <button className="toggle">
+              <i className="fa fa-bars"></i>
+            </button>
+          </nav>
+          <div className="account flexSB">
+            <i className="fa fa-search"></i>
+            <i className="fa fa-bell"></i>
+            {!username ? (
+              <Link to={"/signup"}>
+                {" "}
+                <i className="fa fa-user"></i>
+              </Link>
+            ) : (
+              <h1 className="username-classed" onClick={Logout}>{username}</h1>
+            )}
+            <button>Subscribe Now</button>
           </div>
-          <ul>
-          <Link to='/'>Home</Link>
-          <Link to='/series'>Series</Link>
-          <Link to='/movies'>Movies</Link>
-          <Link to='/contact'>Contact</Link>
-          </ul>
-          <button className='toggle'>
-            <i  className='fa fa-bars'></i>
-          </button>
-        </nav>
-        <div className='account flexSB'>
-          <i className='fa fa-search'></i>
-          <i className='fa fa-bell'></i>
-          <Link to={"/signup"}> <i className='fa fa-user'></i></Link>
-          <button>Subscribe Now</button>
         </div>
-      </div>
-    </header>
-      
+      </header>
     </div>
- 
-  )
-}
+  );
+};
 
 export default Header;
--- src/componets/Homes/Signup.jsx
+++ src/componets/Homes/Signup.jsx
@@ -1,36 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./Signup.css";
-
+import { useNavigate } from "react-router";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+  const [pwd, setPwd] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (true) {
+      localStorage.setItem("username", username);
+      console.log(username);
+      navigate("/");
+    } else return;
+  };
+
+  useEffect(() => {
+    let username = localStorage.getItem("username");
+    if (username) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <>
-    <div className='signup-background'>
-      <div className='signup_header'>
-        <img src="Images/img/sign.jpg" alt='' />
-      
+      <div className="signup-background">
+        <div className="signup_header">
+          <img src="Images/img/sign.jpg" alt="" />
+        </div>
+        <div className="signup_data">
+          <div className="signup_span">
+            <span>Sign In</span>
+          </div>
+          <form onSubmit={submitHandler}>
+            <input
+              type="email"
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Email or Phone Number"
+              required
+            />{" "}
+            <br />
+            <input
+              type="password"
+              onChange={(e) => setPwd(e.target.value)}
+              placeholder="Password"
+              required
+            />
+            <br />
+            <button>Sign In</button>
+          </form>
+          <div className="signup_me">
+            <div className="checkbox">
+              {" "}
+              <input type="checkbox" placeholder="" /> <span>Remember me</span>
+              <a href="https://www.netflix.com/in/login">Need helps?</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='signup_data'>
-      <div className='signup_span'>
-        <span>Sign In</span>
-        </div>
-        <form>
-        <input type='text' placeholder='Email or Phone Number' required /> <br />
-        <input type='password' placeholder='Password' required /><br />
-        <button>Sign In</button>
-        </form>
-        <div className='signup_me'>
-         
-         <div className='checkbox'>   <input type='checkbox' placeholder='' /> <span>Remember me</span>
-         <a href='https://www.netflix.com/in/login'>Need helps?</a></div>
-      
-         
-        </div>
-        </div>
-    </div>
-      
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
